43c71aa6860c9d20fd0bc16fc7356cd518a536fc
fezika18 <ferogerio046@gmail.com>
2026-08-18 11:18:24 -0300

    Contato

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,14 +12,13 @@ export default function Footer() {
     <footer className="footer">
       <div className="container footer-inner">
         <div className="footer-brand">
-          {/* Logo em versão clara para o fundo escuro */}
           <a
             href="/"
             onClick={(e) => { e.preventDefault(); scrollTo('inicio') }}
-            style={{ display: 'inline-block' }}
+            className="footer-logo-link"
           >
             <img
-              src="/logoNavBar.png"
+              src="/logo-footer.png"
               alt="Quelvya"
               className="footer-logo"
             />
@@ -38,7 +37,10 @@ export default function Footer() {
         <div className="footer-col">
           <p className="footer-heading">Contato</p>
           <a href="mailto:contato@quelvya.com.br">contato@quelvya.com.br</a>
-          <a href="tel:+5500000000000">(00) 00000-0000</a>
+          <a href="https://wa.me/+5511985134864">Solange - 11 98513-4864</a>
+          <a href="https://wa.me/+5511969565390">Elidiane - 11 96956-5390</a>
+          <a href="https://www.google.com/maps/place/Av.+Giovanni+Gronchi,+6195+-+Sl+809+-+Vila+Andrade,+S%C3%A3o+Paulo+-+SP,+05724-003/@-23.6336241,-46.7391989,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce5144e210f3cb:0x10f9dce04b1e3f7b!8m2!3d-23.633629!4d-46.736624!16s%2Fg%2F11vqv9jw81?entry=ttu&g_ep=EgoyMDI2MDgxMi4wIKXMDSoASAFQAw%3D%3D">
+            Av. Giovanni Gronchi, 6195 - sala 809 - Vila Andrade, São Paulo - SP, 05724-003, Brasil</a>
         </div>
       </div>
       <div className="container">
